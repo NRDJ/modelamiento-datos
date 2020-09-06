b29f5d0cd2067bed9594dda1258beac69706b07c
Nicolas Riedel <nriedeldj@gmail.com>
2020-09-05 21:23:52 -0400

    Added compound keys to tables prueba_alumnos and alumnos_cursos

--- caso2.sql
+++ caso2.sql
@@ -4,6 +4,6 @@ CREATE TABLE departamentos(id SERIAL PRIMARY KEY, nombre VARCHAR(75) NOT NULL);
 CREATE TABLE profesores(rut VARCHAR(20) PRIMARY KEY, nombre VARCHAR(50) NOT NULL, departamentos_id INT REFERENCES departamentos(id));
 CREATE TABLE pruebas(id SERIAL PRIMARY KEY, puntaje INT NOT NULL, profesores_rut VARCHAR(20) REFERENCES profesores(rut));
 CREATE TABLE alumnos(rut VARCHAR(20) PRIMARY KEY, nombre VARCHAR(50) NOT NULL);
-CREATE TABLE pruebas_alumnos(pruebas_id INT REFERENCES pruebas(id), alumnos_rut VARCHAR(20) REFERENCES alumnos(rut));
+CREATE TABLE pruebas_alumnos(pruebas_id INT REFERENCES pruebas(id), alumnos_rut VARCHAR(20) REFERENCES alumnos(rut) PRIMARY KEY(pruebas_id, alumnos_rut));
 CREATE TABLE cursos(id SERIAL PRIMARY KEY, nombre_asignatura VARCHAR(75) NOT NULL);
-CREATE TABLE alumnos_cursos(cursos_id INT REFERENCES cursos(id), alumnos_rut VARCHAR(20) REFERENCES alumnos(rut));
+CREATE TABLE alumnos_cursos(cursos_id INT REFERENCES cursos(id), alumnos_rut VARCHAR(20) REFERENCES alumnos(rut), PRIMARY KEY(cursos_id,alumnos_rut));
